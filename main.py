6a8543425ed54ae35e17c1f2cf102814555ce692
AngusFaanhof <44198398+AngusFaanhof@users.noreply.github.com>
2020-12-09 21:26:17 +0100

    You can now choose the mode (fast or slow)

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
-import tkinter as tk
-import tkinter.colorchooser as tkc
 from mandelbrot import Mandelbrot
-from helpers import draw_from_iterations_array, hex_from_rgb, get_area_variables
+from helpers import process_iterations_array, get_area_variables, hex_to_rgb
+
+from PIL import Image, ImageTk
+
+import tkinter as tk
+import tkinter.colorchooser as colorchooser
 
 WIDTH = 500
 HEIGHT = 500
 
+pixel_array = []
 color = "#000000"
 
 root = tk.Tk()
 root.title("Project Mandelbrot")
 
+# The image container
+label = tk.Label(root)
 canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT)
-canvas.pack(side=tk.LEFT)
+
+fast_option = tk.BooleanVar()
+fast_option.set(True)
+
+def change_mode():
+    if fast_option.get():
+        label.pack(side=tk.LEFT)
+        canvas.forget()
+    else:
+        canvas.pack(side=tk.LEFT)
+        label.forget()
+
+change_mode()
+
+edge_mandelbrot = tk.Radiobutton(root, text="Fast", variable=fast_option, value=True, command=change_mode)
+edge_mandelbrot.pack()
+
+full_mandelbrot = tk.Radiobutton(root, text="SLOW", variable=fast_option, value=False, command=change_mode)
+full_mandelbrot.pack()
 
 # option select menu
 # this variable contains the selected option. It defaults to 1
@@ -37,11 +61,9 @@ iterations_slider.pack()
 color_label = tk.Label(root, text = "Custom color")
 color_label.pack()
 
-color_button = ""
-
 def change_color():
     global color
-    color = tkc.askcolor()[1]
+    color = colorchooser.askcolor()[1]
     color_button.configure(bg=color)
 
 color_button = tk.Button(root, width=10, command=change_color, text="CLICK ME!")
@@ -51,22 +73,42 @@ color_button.pack()
 zoom_scale = tk.Scale(root, from_=-5, to=5, orient=tk.HORIZONTAL, label='zoom')
 zoom_scale.pack()
 
-# create the initial mandelbrot set
-x_start, y_start, x_width, y_height = get_area_variables(zoom_scale.get())
+def get_mandelbrot(width, height, iterations, zoom):
+    x_start, y_start, x_width, y_height = get_area_variables(zoom)
+    return Mandelbrot(width, height, iterations, x_start, y_start, x_width, y_height)
 
-mandelbrot = Mandelbrot(WIDTH, HEIGHT, iterations_slider.get(), x_start, y_start, x_width, y_height)
-draw_from_iterations_array(canvas, mandelbrot.iterations_array, WIDTH, option.get())
+def change_pixel(pixel, hex_color):
+    pixel_array[pixel] = hex_to_rgb(hex_color)
 
-# Generates a new mandelbrot set and draws it
-def redraw_canvas():
-    x_start, y_start, x_width, y_height = get_area_variables(zoom_scale.get())
-    mandelbrot = Mandelbrot(WIDTH, HEIGHT, iterations_slider.get(),x_start, y_start, x_width, y_height)
+def draw_pixel_on_canvas(pixel, color):
+    x = pixel % WIDTH
+    y = pixel // WIDTH
+    canvas.create_rectangle(x, y, x, y, outline=color)
 
-    canvas.delete("all")
+def redraw_mandelbrot():
+    if fast_option.get():
+        global pixel_array
+        mandelbrot = get_mandelbrot(WIDTH, HEIGHT, iterations_slider.get(), zoom_scale.get())
+        
+        pixel_array = [(255,255,255)] * (WIDTH * HEIGHT)
 
-    draw_from_iterations_array(canvas, mandelbrot.iterations_array, WIDTH, option.get(), color)
+        process_iterations_array(mandelbrot.iterations_array, option.get(), color, change_pixel)
+        
+        image = Image.new(mode="RGB", size=(WIDTH,HEIGHT))
+        image.putdata(pixel_array)
+        photo_image = ImageTk.PhotoImage(image)
 
-button = tk.Button(root, text="Generate", command=redraw_canvas)
-button.pack(side=tk.RIGHT)
+        label.configure(image=photo_image)
+        label.img = photo_image
+    else:
+        mandelbrot = get_mandelbrot(WIDTH, HEIGHT, iterations_slider.get(), zoom_scale.get())
+
+        canvas.delete("all")
+
+        process_iterations_array(mandelbrot.iterations_array, option.get(), color, draw_pixel_on_canvas)
+
+redraw_mandelbrot()
+
+tk.Button(root, text="Generate", command=redraw_mandelbrot).pack()
 
 root.mainloop()
